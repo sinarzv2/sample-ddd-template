29ef20d854c1bcdf9aab8b254dc24e8abef8721d
s.fatemi <s.fatemi@emofid.com>
2023-11-07 16:56:26 +0330

    make unitof work better

--- src/Infrastructure/UnitOfWork/UnitOfWork.cs
+++ src/Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -8,18 +8,20 @@ namespace Infrastructure.UnitOfWork
     internal class UnitOfWork : IUnitOfWork
     {
         private readonly ApplicationDbContext _context;
+        private UserRepository? _userRepository;
+        private RoleRepository? _roleRepository;
+        private UserClaimRepository? _userClaimRepository;
         public UnitOfWork(ApplicationDbContext databaseContext)
         {
             _context = databaseContext;
-            UserRepository = new UserRepository(_context);
-            RoleRepository = new RoleRepository(_context);
-            UserClaimRepository = new UserClaimRepository(_context);
         }
 
 
-        public IUserRepository UserRepository { get; }
-        public IRoleRepository RoleRepository { get; }
-        public IUserClaimRepository UserClaimRepository { get; }
+        public IUserRepository UserRepository => _userRepository ??= new UserRepository(_context);
+
+        public IRoleRepository RoleRepository => _roleRepository ??= new RoleRepository(_context);
+
+        public IUserClaimRepository UserClaimRepository => _userClaimRepository ??= new UserClaimRepository(_context);
 
         public async Task CommitChangesAsync(CancellationToken cancellationToken = default)
         {
